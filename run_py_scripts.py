81fd5b1dba61c6946ee102e2c96b3a0566a26960
Greg Schivley <greg.schivley@gmail.com>
2023-09-22 16:12:46 -0400

    Run scripts in parallel
    Speed up the github action.
    Also clean up the code some.

--- run_py_scripts.py
+++ run_py_scripts.py
@@ -2,33 +2,52 @@ from pathlib import Path
 import subprocess
 import os
 import re
+from joblib import Parallel, delayed
+
+CWD = Path.cwd()
 
 
 def run_script(f: Path):
     subprocess.run([f"python {str(f)}"], shell=True)
 
 
-# Get modified or added files from the last git commit
-result = subprocess.run(
-    ["git", "diff", "--name-only", "HEAD^", "HEAD"], stdout=subprocess.PIPE
-)
-print(result.stdout.decode("utf-8"))
-files = re.split("\n|\t", result.stdout.decode("utf-8"))[:-1]
-
-# Find parent (top level) directories for each of the files
-script_dirs = []
-for f in files:
-    # if Path(f).exists() and len(f) > 1:
-    script_dirs.append(Path(f).parts[0])
-script_dirs = set(script_dirs)
-print(script_dirs)
-
-# Run scripts in the parent directory of the modified files
-cwd = Path.cwd()
-for f in script_dirs:
-    scripts = (cwd / f).glob("figures.py")
+def run_scripts_in_dir(f: Path):
+    scripts = (CWD / f).glob("figures.py")
     for s in scripts:
         os.chdir(s.parent)
         print(s)
         run_script(s)
-        os.chdir(cwd)
+        os.chdir(CWD)
+
+
+def git_changed_files():
+    """
+    Retrieves the names of files that have been modified or added in the last git commit.
+
+    Returns:
+    - A list of file names that have been modified or added in the last git commit.
+    """
+    output = subprocess.check_output(["git", "diff", "--name-only", "HEAD", "HEAD~1"])
+    files = output.decode().splitlines()
+    files = [f.rstrip() for f in files]
+    return files
+
+
+def main():
+    # Get modified or added files from the last git commit
+    files = git_changed_files()
+
+    # Find parent (top level) directories for each of the files
+    script_dirs = []
+    for f in files:
+        # if Path(f).exists() and len(f) > 1:
+        script_dirs.append(Path(f).parts[0])
+    script_dirs = set(script_dirs)
+    print(script_dirs)
+
+    # Run scripts in the parent directory of the modified files
+    Parallel(n_jobs=-1)(delayed(run_scripts_in_dir)(f) for f in script_dirs)
+
+
+if __name__ == "__main__":
+    main()
